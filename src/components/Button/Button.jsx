b0df2560cd68f920377e3c0ccf88d258ab956630
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-12-21 10:16:45 +0300

    add Button Component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Button.module.scss'
+
+function Button() {
+  return (
+    <a href="/" className={styles.button}>عرض تفاصيل</a>
+  )
+}
+
+export default Button
