03a10aa73721cfef9ed7c0fd42253c184bb0e8ce
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-12-23 13:30:00 +0300

    update app Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import './App.scss';
+import React, { useState } from 'react';
 
+import './App.scss';
+import Sidebar from './components/Sidebar/Sidebar';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Filter from './components/Filter/Filter';
 function App() {
-  return <div className="App">Welcome</div>;
+  const [isNavOpen, setIsNavOpen] = useState(true);
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+  return (
+    <div className="App">
+      <Header isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+      <Sidebar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+      <Main
+        setIsFilterOpen={setIsFilterOpen}
+        isFilterOpen={isFilterOpen}
+        isNavOpen={isNavOpen}
+      />
+      {isFilterOpen && <Filter setIsFilterOpen={setIsFilterOpen} />}
+    </div>
+  );
 }
 
 export default App;
