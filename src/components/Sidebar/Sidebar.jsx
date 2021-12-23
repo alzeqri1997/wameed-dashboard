57351856de56978ce78fa33a5e2c738a500fa073
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-12-23 13:10:00 +0300

    Update Sidebar Component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import styles from './Sidebar.module.scss'
 import { LogoIcon, HouseIcon, BriefcaseIcon, SettingIcon, TeacherIcon, MessageIcon, SMSIcon, LoginIcon } from '../../icons'
 
-function Sidebar({ isNavOpen }) {
+function Sidebar({ isNavOpen, setIsNavOpen }) {
+  const handleClick = () => {
+    setIsNavOpen(prev => !prev)
+  }
   return (
     <div className={`${styles.l_navbar} ${isNavOpen ? '' : 'nav-hide'}`} >
       <nav className={styles.nav} >
@@ -11,9 +14,11 @@ function Sidebar({ isNavOpen }) {
             <LogoIcon className={styles.icon} />
           </a>
 
-          {/* <div className={styles.toggle} >
-            <BarsIcon />
-          </div> */}
+          <button onClick={handleClick} className={styles.close_icon} >
+            <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 14">
+              <text id="_" data-name="" transform="translate(14 12)" fill="#333" font-size="14" font-family="FontAwesome6Pro-Regular, 'Font Awesome \36  Pro'"> <tspan x="-4.375" y="0"></tspan></text>
+            </svg>
+          </button>
 
           <ul className={`${styles.list} text-end `} >
             <a href="/" className={`${styles.link} active `} >
@@ -45,7 +50,7 @@ function Sidebar({ isNavOpen }) {
 
         <a href="/" className={styles.link_bottom}>
           <LoginIcon />
-          <span className={styles.link}> تسجيل الخروج </span>
+          <span> تسجيل الخروج </span>
         </a>
       </nav>
     </div>
