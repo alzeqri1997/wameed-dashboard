2ebd3f7cdbd57fea3f288c14e1529aaa06ea02a0
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-12-23 13:14:39 +0300

    update SearchBar Component

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import styles from './SearchBar.module.scss'
 import { SearchIcon } from '../../icons'
 
-function SearchBar() {
+function SearchBar({ setIsInputOpen, isInputOpen, setIsMenuOpen }) {
+
+
+
   return (
-    <div className={styles.searchBar} >
-      <input className={styles.inputTag} placeholder="هل تبحث عن شئ ..." type="search" />
-      <i className={styles.searchIcon} >
-        <SearchIcon />
-      </i>
-    </div>
+    <>
+      <div style={{ transform: 'translateY( -100px);' }} className={`${styles.searchBar} ${isInputOpen ? 'open' : ''} `}>
+        <input className={styles.inputTag} placeholder="هل تبحث عن شئ ..." type="search" />
+        <i className={styles.searchIcon} >
+          <SearchIcon />
+        </i>
+      </div>
+
+
+
+    </>
   )
 }
 
